437f8d8f03a3233fc2e82daca4d51b9d66ce0cd7
SIholin <iholin.sanna@gmail.com>
2019-05-30 14:04:16 +0300

    heroku toimimaan

--- application/__init__.py
+++ application/__init__.py
@@ -3,8 +3,13 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///auction.db"
-app.config["SQLALCHEMY_ECHO"] = True
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///auction.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -21,9 +26,9 @@ from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager
+
 login_manager = LoginManager()
 login_manager.init_app(app)
-
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
@@ -31,4 +36,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
